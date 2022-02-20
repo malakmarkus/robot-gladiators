2bc81cac7d3fbcfafe4c881460d14c0893974c84
Malak Markus <malak.markus@outlook.com>
2022-02-20 00:27:04 -0500

    add index.html and game.js files

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+var playerName = window.prompt("What is your robot's name?")
+console.log (playerName)
+// this creats a function called "fight"
+function fight() {
+    window.alert("the fight had begun");
+}
+// fight();
